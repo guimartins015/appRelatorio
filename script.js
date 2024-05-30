6e2cd07d26e3a9151319c39bbedd2f7e4182b678
guimartins05 <guimartins15@hotmail.com>
2024-05-30 12:08:33 -0300

    Aplicativo 2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 if("serviceWorker" in navigator){
   navigator.serviceWorker
   .register("./sw.js")
-  .then((reg) => console.log("Registro de SW exitoso",reg))
+  .then((reg) => console.log("Registro de SW Bem-sucedido",reg))
   .catch((err) => console.log("Erro ao tentar registrar SW", err));
 }else{
    console.log("Não há serviceWorker")
